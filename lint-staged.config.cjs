bdf009f99b2b6c5c10720793c5fd0beaaa456628
Arseniy Ivanov <freeatnet@freeatnet.com>
2023-04-24 17:23:02 -0400

    set up husky and lint-staged

---
+++ lint-staged.config.cjs
@@ -0,0 +1,17 @@
+const path = require("path");
+
+/** @type {import("lint-staged").Config} */
+const config = {
+  "*.{js,cjs,mjs,jsx,ts,tsx}": (filenames) => [
+    // Lint TypeScript and JavaScript files
+    // cf. https://github.com/okonet/lint-staged#integrate-with-nextjs
+    `yarn lint:precommit --file ${filenames
+      .map((f) => path.relative(process.cwd(), f))
+      .join(" --file ")}`,
+
+    // Check types for the entire project
+    "tsc -p tsconfig.json --noEmit",
+  ],
+};
+
+module.exports = config;
