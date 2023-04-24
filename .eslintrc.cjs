10bb3d2f9e247fc789f569987eba1a06b6078b42
Arseniy Ivanov <freeatnet@freeatnet.com>
2023-04-24 17:22:34 -0400

    stop trying to lint CJS files as if they were TypeScript

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,43 +3,43 @@ const path = require("path");
 
 /** @type {import("eslint").Linter.Config} */
 const config = {
+  extends: [
+    "next/core-web-vitals",
+    "plugin:tailwindcss/recommended",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["@typescript-eslint"],
   overrides: [
     {
       extends: [
+        "plugin:@typescript-eslint/recommended",
         "plugin:@typescript-eslint/recommended-requiring-type-checking",
       ],
       files: ["*.ts", "*.tsx"],
       parserOptions: {
         project: path.join(__dirname, "tsconfig.json"),
+        ecmaFeatures: {
+          jsx: true,
+        },
+        warnOnUnsupportedTypeScriptVersion: true,
       },
       rules: {
         "@typescript-eslint/consistent-type-assertions": [
           "error",
           { assertionStyle: "never" },
         ],
+        "@typescript-eslint/consistent-type-imports": [
+          "warn",
+          {
+            prefer: "type-imports",
+            fixStyle: "inline-type-imports",
+          },
+        ],
         "@typescript-eslint/no-unnecessary-condition": "error",
       },
     },
   ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: path.join(__dirname, "tsconfig.json"),
-  },
-  plugins: ["@typescript-eslint"],
-  extends: [
-    "next/core-web-vitals",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:tailwindcss/recommended",
-    "plugin:prettier/recommended",
-  ],
   rules: {
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      {
-        prefer: "type-imports",
-        fixStyle: "inline-type-imports",
-      },
-    ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     "import/first": "error",
     "import/no-duplicates": "error",
