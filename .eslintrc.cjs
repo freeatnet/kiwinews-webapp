6a06a146eb7e0ba3c1161bf2201773f0b97818ec
Arseniy Ivanov <freeatnet@freeatnet.com>
2023-04-24 16:15:52 -0400

    replace prettier-plugin-tailwindcss with eslint-plugin-tailwindcss

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,7 +19,11 @@ const config = {
     project: path.join(__dirname, "tsconfig.json"),
   },
   plugins: ["@typescript-eslint"],
-  extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+  extends: [
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:tailwindcss/recommended",
+  ],
   rules: {
     "@typescript-eslint/consistent-type-imports": [
       "warn",
