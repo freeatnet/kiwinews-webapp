18de1f92c1d526fbdd25709aeef4a9b027cad5ab
MacB <mac@macbudkowski.com>
2023-05-21 19:22:33 +0200

    made buttons clickable

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,5 +1,5 @@
 import { TopNav } from "~/layout";
-
+import { ConnectButtonCustom } from "~/layout/ConnectButtonCustom";
 // Constant for freeatnet to call the API - we get all community members scores and organize them like here https://news.kiwistand.com/community
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -20,25 +20,29 @@ const Profile = () => {
       <TopNav />
       <div className="mx-auto mb-8 max-w-5xl px-4 pt-4 text-center text-xl font-bold lg:px-0">
         <p>
-          To see your Profile and collect your upvotes and submissions you need
-          to connect wallet that holds Kiwi NFT.
+          To create your Profile, check your kiwi score and see all your upvotes
+          and submissions you need to connect wallet that owns a Kiwi NFT.
         </p>
         <br />
         <hr className="mx-auto my-4 w-1/4 bg-gray-400 text-left" />
         <div className="mx-auto mb-4 max-w-5xl px-4 pt-4 text-center text-base lg:px-0">
           <p> If you already have Kiwi NFT you can connect your wallet here:</p>
         </div>
-        <button className="w-1/2 bg-black px-4 py-2 text-base text-white hover:bg-kiwi">
-          Connect Wallet
-        </button>
+        <ConnectButtonCustom className="w-1/2 bg-black px-4 py-2 text-base text-white hover:bg-blue-700"></ConnectButtonCustom>
         <br />
         <br />
         <div className="mx-auto mb-4 max-w-5xl px-4 pt-4 text-center text-base lg:px-0">
           <p> And if you don’t have Kiwi NFT yet you can mint it here:</p>
         </div>
-        <button className="w-1/2 bg-kiwi px-4 py-2 text-base text-white hover:bg-blue-700">
-          Mint Kiwi NFT for 0.005Ξ (on Zora)
-        </button>
+        <a
+          href="https://zora.co/collect/0xebb15487787cbf8ae2ffe1a6cca5a50e63003786"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className="w-1/2 bg-kiwi px-4 py-2 text-base text-white hover:bg-blue-700">
+            Mint Kiwi NFT for 0.005Ξ (on Zora)
+          </button>
+        </a>
       </div>
     </>
   );
