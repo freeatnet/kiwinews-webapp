e201bd3d2b18dcb0b8098d113a6d59649f56a3bd
Arseniy Ivanov <freeatnet@freeatnet.com>
2023-05-21 22:28:42 +0300

    chore: add tailwind-debug-screens utility

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,19 @@
+import classNames from "classnames";
 import { Html, Head, Main, NextScript } from "next/document";
+
+import { env } from "~/env.mjs";
 
 export default function Document() {
   return (
     <Html>
       <Head />
       <body>
-        <div className="bg-ivory">
+        <div
+          className={classNames(
+            "bg-ivory",
+            env.NODE_ENV === "development" && "debug-screens"
+          )}
+        >
           <Main />
           <NextScript />
         </div>
