cdd3bda19f1777c76365884c3753179f6135e86d
Arseniy Ivanov <138289+freeatnet@users.noreply.github.com>
2023-09-22 18:59:30 +0300

    chore: switch to `@web3modal/wagmi@3.0.0-beta` (#112)

---
+++ src/features/wallet/config.ts
@@ -0,0 +1,60 @@
+import { walletConnectProvider } from "@web3modal/wagmi";
+import { configureChains, createConfig } from "wagmi";
+import { mainnet, optimism } from "wagmi/chains";
+import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
+import { InjectedConnector } from "wagmi/connectors/injected";
+import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+import { env } from "~/env.mjs";
+
+const {
+  NEXT_PUBLIC_RAINBOW_APP_NAME: RAINBOW_APP_NAME,
+  NEXT_PUBLIC_ALCHEMY_API_KEY: ALCHEMY_API_KEY,
+  NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID: WALLETCONNECT_PROJECT_ID,
+} = env;
+
+export const chains = [mainnet, optimism];
+
+const { publicClient } = configureChains(
+  chains,
+  [
+    walletConnectProvider({
+      projectId: WALLETCONNECT_PROJECT_ID,
+    }),
+    alchemyProvider({
+      apiKey: ALCHEMY_API_KEY,
+    }),
+    publicProvider(),
+  ],
+  {
+    batch: {
+      multicall: true,
+    },
+  }
+);
+
+export const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors: [
+    new WalletConnectConnector({
+      chains,
+      options: {
+        projectId: WALLETCONNECT_PROJECT_ID,
+        showQrModal: false,
+      },
+    }),
+    new InjectedConnector({
+      chains,
+      options: { shimDisconnect: true },
+    }),
+    new CoinbaseWalletConnector({
+      chains,
+      options: {
+        appName: RAINBOW_APP_NAME,
+      },
+    }),
+  ],
+  publicClient,
+});
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,16 @@
-import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { type AppType } from "next/app";
 import { WagmiConfig } from "wagmi";
 
-import { chains, wagmiConfig } from "~/features/wallet";
+import { wagmiConfig } from "~/features/wallet/config";
+import "~/features/wallet/createWeb3Modal";
 import { api } from "~/utils/api";
 
-import "@rainbow-me/rainbowkit/styles.css";
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider chains={chains}>
-        <Component {...pageProps} />
-      </RainbowKitProvider>
+      <Component {...pageProps} />
     </WagmiConfig>
   );
 };
